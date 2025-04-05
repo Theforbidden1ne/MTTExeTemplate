cf11c1bba436a0052aec6de489810b4ee1a9cafd
Theforbidden1ne <mcwhorterthomas65@gmail.com>
2025-04-05 00:10:53 -0700

    Update init.bat

--- init.bat
+++ init.bat
@@ -2,7 +2,6 @@
 chcp 65001 >nul
 cls
 :mainset
-title Multi Tool - Currently Logged in as [%user%]
 cls
 echo.
 echo ╔═══════════════════════════════════════════════╗
@@ -19,23 +18,4 @@ echo ║                                               ║
 echo ║                                               ║
 echo ╚═══════════════════════════════════════════════╝
 echo.
-
-set /p a=Select an option [%user%]: 
-if "%a%"=="1" goto one
-
-if "%a%"=="2" goto two
-
-echo [!] Invalid Choice. Please try again... [!]
 pause
-goto success
-
-:one
-start https://www.google.com/
-goto success
-
-:two
-cls
-echo Made by Tolerate
-timeout 2 >nul
-pause
-goto success
