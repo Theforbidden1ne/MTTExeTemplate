14d6ea6aa1d2b3c8846cff7c5c3b6039facd90ed
Theforbidden1ne <mcwhorterthomas65@gmail.com>
2025-04-05 00:00:55 -0700

    Create init.bat

---
+++ init.bat
@@ -0,0 +1,38 @@
+:mainset
+title Multi Tool - Currently Logged in as [%user%]
+cls
+echo.
+echo ╔═══════════════════════════════════════════════╗
+echo ║                                               ║
+echo ║                                               ║
+echo ║                                               ║
+echo ║                                               ║
+echo ║                                               ║
+echo ║                                               ║
+echo ║                                               ║
+echo ║                                               ║
+echo ║                                               ║
+echo ║                                               ║
+echo ║                                               ║
+echo ╚═══════════════════════════════════════════════╝
+echo.
+
+set /p a=Select an option [%user%]: 
+if "%a%"=="1" goto one
+
+if "%a%"=="2" goto two
+
+echo [!] Invalid Choice. Please try again... [!]
+pause
+goto success
+
+:one
+start https://www.google.com/
+goto success
+
+:two
+cls
+echo Made by Tolerate
+timeout 2 >nul
+pause
+goto success
